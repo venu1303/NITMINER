3cfe6b080a4af6036c9cbeff4c661c2424bda690
Venu1303 <venuthegreat2@gmail.com>
2023-09-21 02:06:28 +0530

    Initial commit

---
+++ src/Cards.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Services</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src="images/i1.jpg"
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src='images/i2.jpeg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/i3.jpeg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='images/i4.jpeg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/i5.jpeg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
